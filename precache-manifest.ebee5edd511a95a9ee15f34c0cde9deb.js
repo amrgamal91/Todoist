da7fa499b011aa192b891f1c909d57a33745e965
gemy91 <ag.amrgamal91@gmail.com>
2019-11-26 07:19:55 +0300

    Updates

---
+++ precache-manifest.ebee5edd511a95a9ee15f34c0cde9deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6fc6f95e05e488444870c2ca7caa3a",
+    "url": "/Todoist/index.html"
+  },
+  {
+    "revision": "c6d1b4bd4d6bcc4404c8",
+    "url": "/Todoist/static/css/main.97d9f8f3.chunk.css"
+  },
+  {
+    "revision": "9dac7e2292be2cbae0b6",
+    "url": "/Todoist/static/js/2.fbd35c60.chunk.js"
+  },
+  {
+    "revision": "c6d1b4bd4d6bcc4404c8",
+    "url": "/Todoist/static/js/main.614b43d6.chunk.js"
+  },
+  {
+    "revision": "963e4078c7233ca08f27",
+    "url": "/Todoist/static/js/runtime-main.405d7779.js"
+  }
+]);
